1adbd7db20753d2c1e34d0991f605c499a3fea33
山本英明 <yamamotohideaki@yamamotohideakinoMacBook-Air.local>
2021-09-20 21:39:41 +0900

    apply query and variables

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import { useState } from "react";
 import { ApolloProvider } from "react-apollo";
 import { Query } from "react-apollo";
 import client from "./client";
-import { ME } from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
 
-function App() {
+// useStateの持つ初期値
+const initialVariables = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア",
+};
+
+export const App = (props) => {
+  const [state, setState] = useState(initialVariables);
+
+  // もし取得した値が渡ってきたらどうするかがわからない
+  console.log(state);
+  const { propsVariables } = props;
+
   return (
     <ApolloProvider client={client}>
-      <div className="App">hello, GraphQL</div>
-      <Query query={ME}>
+      <Query query={SEARCH_REPOSITORIES} variables={state}>
         {({ loading, error, data }) => {
+          console.log({ data });
           if (loading) return "loading...~_~...";
           if (error) return `Error!!!おらぁ!!! ${error.message}`;
 
-          return <div>{data.user.name}</div>;
+          return <div></div>;
         }}
       </Query>
     </ApolloProvider>
   );
-}
+};
 
 export default App;
